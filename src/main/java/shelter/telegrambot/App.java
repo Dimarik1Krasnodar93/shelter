a3f56d3144b7958a9aa9ef4a53cd0c56ee840280
Dimarik <krasnodar93@mail.ru>
2023-03-19 19:37:46 +0300

    3-phase

--- src/main/java/shelter/telegrambot/App.java
+++ src/main/java/shelter/telegrambot/App.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class App 
-{//2-phase-3
+{//3-phase-!!!
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
