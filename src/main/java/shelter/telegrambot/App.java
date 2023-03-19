bf0e07007aa82b8e3df6af0fe9d06ba357981265
Dimarik <krasnodar93@mail.ru>
2023-03-19 15:12:58 +0300

    phase 1

--- src/main/java/shelter/telegrambot/App.java
+++ src/main/java/shelter/telegrambot/App.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class App 
-{//1
+{//3/13
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
